a52ad31b00666e2d9ac56dda4c1e9f05e6e027fc
Steven Quintana <steve.e.quint@gmail.com>
2021-08-11 21:06:02 -0700

    Add event listeners to close buttons to close modal

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 // Function that toggles pop up modal on button click.
 document.getElementById('generateBtn').addEventListener('click', event => {
   event.preventDefault()
+
   document.getElementById('generatorPrompts').innerHTML = `
-        <div class="promptsModal">
+        <div class="promptsModal" id="closeModalOuter">
           <div class="modal-dialog">
             <div class="modal-content modalPosition">
               <div class="modal-header">
                 <h5 class="modal-title">Modal title</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" id="closeModalCorner"></button>
               </div>
               <div class="modal-body">
                 <p>Modal body text goes here.</p>
               </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary" id="closeModalBottom">Close</button>
                 <button type="button" class="btn btn-primary">Save changes</button>
               </div>
             </div>
           </div>
         </div>
       `
+  // Close modal on corner 'x' button click.
+  document.getElementById('closeModalCorner').addEventListener('click', event => {
+    event.preventDefault();
+
+    document.getElementById('generatorPrompts').innerHTML = ''
+  })
+  // Close modal on lower close button click.
+  document.getElementById('closeModalBottom').addEventListener('click', event => {
+    event.preventDefault();
+
+    document.getElementById('generatorPrompts').innerHTML = ''
+
+  })
+  // Close modal on out of window click.
+  document.getElementById('closeModalOuter').addEventListener('click', event => {
+    event.preventDefault();
+
+    document.getElementById('generatorPrompts').innerHTML = ''
+  })
 })
