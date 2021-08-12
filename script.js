73a352089c34c147ad2ce4f5a91f2d32f2c06671
Steven Quintana <steve.e.quint@gmail.com>
2021-08-11 20:40:24 -0700

    Move script back into it's own file

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function that toggles pop up modal on button click.
+document.getElementById('generateBtn').addEventListener('click', event => {
+  event.preventDefault()
+  document.getElementById('generatorPrompts').innerHTML = `
+        <div class="testDiv"></div>
+      `
+})
