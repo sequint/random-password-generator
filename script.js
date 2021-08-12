e6cd6d692481325bdbc77acb13e26751805b74f4
Steven Quintana <steve.e.quint@gmail.com>
2021-08-11 20:56:18 -0700

    Add top margin to modal window

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('generateBtn').addEventListener('click', event => {
   document.getElementById('generatorPrompts').innerHTML = `
         <div class="promptsModal">
           <div class="modal-dialog">
-            <div class="modal-content">
+            <div class="modal-content modalPosition">
               <div class="modal-header">
                 <h5 class="modal-title">Modal title</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
