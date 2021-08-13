215da89442d98b8778540b2108b240e7f5c28a2e
Steven Quintana <steve.e.quint@gmail.com>
2021-08-13 08:41:34 -0700

    Create second version with prompts

---
+++ script2.js
@@ -0,0 +1,10 @@
+document.getElementById('generate').addEventListener('click', () => {
+
+  document.getElementById('generate').addEventListener('click', () => {
+    
+    let passLength = prompt('Enter password length (8-128)')
+    let 
+
+  })
+
+})
